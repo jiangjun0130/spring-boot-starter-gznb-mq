25c0d8e0031ebd4bb5494b6d73525d6ec9adb666
jj <jiangjun@guozi>
2017-12-26 14:08:00 +0800

    first commit

---
+++ src/main/java/com/gznb/mq/kafka/listener/AbstractKafkaConsumerListener.java
@@ -0,0 +1,40 @@
+package com.gznb.mq.kafka.listener;
+
+import com.gznb.mq.kafka.consumer.ConsumerAutoConfiguration;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.KafkaException;
+import java.lang.reflect.ParameterizedType;
+
+/**
+ * kafka consumer listener抽象基类
+ *
+ * @author jiangjun
+ * @create 2017/11/22
+ */
+@Deprecated
+public abstract class AbstractKafkaConsumerListener<T> {
+
+    protected Logger logger = null;
+
+    public AbstractKafkaConsumerListener(){
+//        try {
+//            ParameterizedType pt = (ParameterizedType) this.getClass().getGenericSuperclass();
+//            Class<T> clazz = (Class<T>) pt.getActualTypeArguments()[0];
+//            logger = LoggerFactory.getLogger(clazz);
+//        }catch (Exception e){
+//            throw new KafkaException("AbstractKafkaConsumerListener获取子类类型异常！");
+//        }
+    }
+
+    /**
+     * 子类监听器必须要实现该抽象方法
+     * 子类需要在该方法上加入注解：
+     *  @KafkaListener(topics = {"test"})。
+     *  默认采用{@link ConsumerAutoConfiguration}自动装配的 KafkaListenerContainerFactory bean
+     * @param record
+     */
+    public abstract void listen(ConsumerRecord<?,?> record) throws KafkaException;
+
+}
---
+++ src/main/java/com/gznb/mq/kafka/listener/IKafkaConsumerListener.java
@@ -0,0 +1,23 @@
+package com.gznb.mq.kafka.listener;
+
+import com.gznb.mq.kafka.consumer.ConsumerAutoConfiguration;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.KafkaException;
+
+/**
+ * kafka consumer listener interface
+ *
+ * @author jiangjun
+ * @create 2017/11/22
+ */
+public interface IKafkaConsumerListener {
+
+    /**
+     * consumer监听器必须要实现该接口，并将类纳入spring容器进行管理。即可接收kafka订阅的消息
+     * 子类需要在该方法上加入注解：
+     *  @KafkaListener(topics = {"test"})。
+     *  默认采用{@link ConsumerAutoConfiguration}自动装配的 KafkaListenerContainerFactory bean
+     * @param record
+     */
+    void listen(ConsumerRecord<?,?> record) throws KafkaException;
+}
